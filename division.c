36a2d12af2ca35d84d3621c113e31459a8045520
prathyushakasturi29-2005 <prathyushakasturi29@gmail.com>
2025-11-27 11:42:56 +0530

    division.c

---
+++ division.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter a value:");
+    scanf("%d",&a);
+    printf("Enter b value:");
+    scanf("%d",&b);
+    c=a/b;
+    printf("Division of two numbers:%d",c);
+    return 0;
+}
