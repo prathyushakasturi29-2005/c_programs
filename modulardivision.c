023dd902a14a2ecac4ee8cd39ae2728db26d546e
prathyushakasturi29-2005 <prathyushakasturi29@gmail.com>
2025-11-27 12:12:57 +0530

    modulardivision.c

---
+++ modulardivision.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,mod_div;
+    printf("Enter num1:");
+    scanf("%d",&num1);
+    printf("Enter num2:");
+    scanf("%d",&num2);
+    mod_div=num1%num2;
+    printf("Modular division of two numbers:%d",mod_div);
+    return 0;
+}
+
+   
