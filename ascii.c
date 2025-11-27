6efb03a59a61fd4c1235afc05dd91ade22cd006e
prathyushakasturi29-2005 <prathyushakasturi29@gmail.com>
2025-11-27 15:12:42 +0530

    ascii.c

---
+++ ascii.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("Enter character:");
+scanf("%c",&ch);
+printf("The ASCII value of %C is:%d\n",ch,ch);
+int n;
+printf("Enter value:");
+scanf("%d",&n);
+printf("The character of ASCII value is:%c",n,n);
+return 0;
+}
