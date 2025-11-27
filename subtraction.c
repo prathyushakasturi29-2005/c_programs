15adefb0f5b2bcf5727514f4bd3b6175514d0cf8
prathyushakasturi29-2005 <prathyushakasturi29@gmail.com>
2025-11-27 12:09:26 +0530

    subtraction.c

--- subtraction.c
+++ subtraction.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c;
-    printf("Enter a value:");
-    scanf("%d",&a);
-    printf("Enter b value:");
-    scanf("%d",&b);
-    c=a-b;
-    printf("Subtraction of two numbers:%d",c);
+    int num1,num2,sub;
+    printf("Enter num1:");
+    scanf("%d",&num1);
+    printf("Enter num2:");
+    scanf("%d",&num2);
+    sub=num1-num2;
+    printf("Subtraction of two numbers:%d",sub);
     return 0;
 }
+
+   
