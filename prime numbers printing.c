de23ab8cf99c24962b49bb961b4205a7b21dcf99
prathyushakasturi29-2005 <prathyushakasturi29@gmail.com>
2025-12-05 22:51:30 +0530

    prime numbers printing.c

---
+++ prime numbers printing.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,isPrime;
+    printf("Enter a limit:");
+    scanf("%d",&n);
+    printf("Enter the numbers between 1 and %d are:\n",n);
+    for(i=2;i<=n;i++)
+    {
+    isPrime=1;
+    for(j=2;j<=i/2;j++)
+    {
+        if(i%j==0)
+        {
+            isPrime=0;
+            break;
+        }
+    }
+    if(isPrime==1)
+    {
+        printf("%d",i);
+    }
+}
+return 0;
+}
