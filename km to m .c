b9547afe3f538996e33cba4ea112caec3008a55a
prathyushakasturi29-2005 <prathyushakasturi29@gmail.com>
2025-12-19 21:02:22 +0530

    km to m .c

---
+++ km to m .c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    float km, meters;
+    printf("Enter distance in kilometers:");
+    scanf("%f",&km);
+    meters=km*1000;
+    printf("%.2f kilometers=%.2f meters\n",km,meters);
+    return 0;
+}
