9486f6cd52a33ee82de78e88aa738437ff41786b
prathyushakasturi29-2005 <prathyushakasturi29@gmail.com>
2025-12-19 15:09:30 +0530

    Two numbers equal or not using assignment operator.c

---
+++ Two numbers equal or not using assignment  operator.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter two numbers:");
+    scanf("%d %d",&a,&b);
+    if(a==b)
+    {
+        printf("Both numbers are equal");
+    }
+    else
+    {
+        printf("Both numbers are not equal");
+    }
+    return 0;
+}
+
+    
