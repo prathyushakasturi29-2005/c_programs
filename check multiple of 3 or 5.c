dfa0db090edefab12a56a4c7a0d5774d9a69a5d5
prathyushakasturi29-2005 <prathyushakasturi29@gmail.com>
2025-12-20 12:08:40 +0530

    check multiple of 3 or 5.c

---
+++ check multiple of 3 or 5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    if (num% 3==0||num%5==0)
+    {
+        printf("%d is a multiple of 3 or 5\n",num);
+    }
+    else
+    {
+        printf("%d is not a multiple of 3 or 5\n",num);
+    }
+    return 0;
+}
